ee95d63b3f9759a25182d6c418cc6f5829082e1f
OptimalBlock489 <89468732+OptimalBlock489@users.noreply.github.com>
2023-04-01 02:59:29 +0530

    Remove welcome message.

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 const chatBody = document.getElementById("chat-body");
 const messageInput = document.getElementById("message-input");
 const sendButton = document.getElementById("send-button");
-let initbotMessage;
-window.onload = async function () {
-  const url = "https://api.openai.com/v1/chat/completions";
-  const data = {
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a mental health support chat bot. You have been programmed to send a text to an user to check their mental health. You have to start the converstaion and behave friendly. You have to be the user's friend and listen to everything the user has to offer in order to help his or her mental state.  Your task is to determine the mental state of the user, and helping them to overcome it. If you think they have any mental disorder then say it upright clearly. Give them solutions to cope up with it.\nNow generate a check up message to the user.",
-      },
-    ],
-    max_tokens: 150,
-    temperature: 0.2,
-    n: 1,
-    frequency_penalty: 1,
-  };
-  try {
-    const response = await axios.post(url, data, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bear" + "er " + "s" + "k-vJ" + "qpLxibDjzCJK41HBlA"+"T3BlbkFJjtF"+"WnWcQ11r5phjTsVvs",
-      },
-    });
-    initbotMessage = response.data.choices[0].message.content.trim();
-    displayMessage(initbotMessage, "bot-message");
-  } catch (error) {
-    console.error(error);
-  }
   // Send message when user clicks send button
   sendButton.addEventListener("click", sendMessage);
 
@@ -87,4 +57,3 @@ window.onload = async function () {
     chatBody.appendChild(messageElement);
     chatBody.scrollTop = chatBody.scrollHeight;
   }
-};
